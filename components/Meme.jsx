46cc0f75883cd16bf8b3da6f4be055d8ce640769
HoangT <ac5217@student.jamk.fi>
2022-07-22 00:26:40 +0300

    generator photos

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import memesData from "../memesData";
 
 function Meme() {
+  const [memeImage, setMemeImage] = React.useState("");
+  function getRandomImage() {
+    const memesArray = memesData.data.memes;
+    const getRandomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[getRandomNumber].url);
+  }
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <input type="text" placeholder="Top text" className="form--input" />
         <input type="text" placeholder="Bottom text" className="form--input" />
-        <button className="form--button">Get a new meme image 🖼</button>
-      </form>
+        <button className="form--button" onClick={getRandomImage}>
+          Get a new meme image 🖼
+        </button>
+      </div>
+      <img src={memeImage} className="meme-image" />
     </main>
   );
 }

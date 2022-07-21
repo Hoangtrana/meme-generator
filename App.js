28603a3d2b72f1547599d1002ea31f889b412344
HoangT <ac5217@student.jamk.fi>
2022-07-21 14:19:12 +0300

    adding meme.jsx and style

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
+import Meme from "./components/Meme";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
+        <Meme />
       </header>
     </div>
   );
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import TrollFace from "../images/TrollFace.png";
 function Navbar() {
   return (
     <nav className="navbar">
-      <img src="./images/troll-face.png" className="navbar--logo" />
+      <img src={TrollFace} alt="troll face" className="navbar--logo" />
       <h1 className="navbar--header">Meme Generator</h1>
       <p>React Course - Project 3</p>
     </nav>

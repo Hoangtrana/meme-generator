c58597fa68a38b0eca47cfdd40b71d0767cbc437
HoangT <ac5217@student.jamk.fi>
2022-07-21 13:48:48 +0300

    adding navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <img src="./images/troll-face.png" className="navbar--logo" />
+      <h1 className="navbar--header">Meme Generator</h1>
+      <p>React Course - Project 3</p>
+    </nav>
+  );
+}
+
+export default Navbar;
